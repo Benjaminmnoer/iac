95b2fa40dfd5fbafbd8c9ff3debd23dc117a2395
Benjamin Noer <benjaminmnoer25@gmail.com>
2025-01-18 13:23:46 +0100

    Moved talos IPs to  variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,11 @@
+#################### GENERAL ####################
+variable "default_gateway" {
+  description = "Default gateway IP address"
+  type = string
+  default = "192.168.50.1"
+}
+
+#################### PROXMOX ####################
 variable "virtual_environment_endpoint" {
   type = string
 }
@@ -10,8 +18,18 @@ variable "virtual_environment_password" {
   type = string
 }
 
-variable "default_gateway" {
-  description = "Default gateway IP address"
-  type = string
-  default = "192.168.50.1"
+#################### TALOS ####################
+variable "talos_cluster_name" {
+  type    = string
+  default = "istari"
+}
+
+variable "talos_cp_01_ip" {
+  type    = string
+  default = "192.168.50.101"
+}
+
+variable "talos_worker_01_ip" {
+  type    = string
+  default = "192.168.50.102"
 }
