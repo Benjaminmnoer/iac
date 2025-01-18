2e4af2b49e3101f70a3a210ce49db6373545f79e
Benjamin Noer <benjaminmnoer25@gmail.com>
2025-01-18 11:36:03 +0100

    Added cluster options

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,25 +1,12 @@
 resource "proxmox_virtual_environment_cluster_options" "options" {
   language                  = "en"
   keyboard                  = "da"
-  email_from                = "root@$hostname"
+  email_from                = "pve@benjaminmnoer.dk"
   max_workers               = 5
 #   migration_cidr            = "10.0.0.0/8"
 #   migration_type            = "secure"
   next_id = {
-    lower = 100
+    lower = 200
     upper = 999999999
-  }
-}
-
-resource "proxmox_virtual_environment_cluster_firewall" "example" {
-  enabled = false
-
-  ebtables      = false
-  input_policy  = "DROP"
-  output_policy = "ACCEPT"
-  log_ratelimit {
-    enabled = false
-    burst   = 10
-    rate    = "5/second"
   }
 }
