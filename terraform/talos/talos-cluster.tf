4f05bc4b9389efcbd5b49bda589b919b23e0076b
Benjamin Noer <benjaminmnoer25@gmail.com>
2025-01-19 18:49:58 +0100

    Configured talos cluster

--- terraform/talos/talos-cluster.tf
+++ terraform/talos/talos-cluster.tf
@@ -3,57 +3,73 @@ resource "talos_machine_secrets" "machine_secrets" {}
 data "talos_client_configuration" "talosconfig" {
   cluster_name         = var.talos_cluster_name
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  endpoints            = [var.talos_cp_01_ip]
+  endpoints            = [var.talos_cp_01.ip, var.talos_cp_02.ip]
 }
 
 data "talos_machine_configuration" "machineconfig_cp" {
   cluster_name     = var.talos_cluster_name
-  cluster_endpoint = "https://${var.talos_cp_01_ip}:6443"
+  cluster_endpoint = "https://istari.benjaminmnoer.dk"
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
 
-resource "talos_machine_configuration_apply" "cp_config_apply" {
+resource "talos_machine_configuration_apply" "cp_01_config_apply" {
   depends_on                  = [ proxmox_virtual_environment_vm.talos_cp_01 ]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.machineconfig_cp.machine_configuration
   count                       = 1
-  node                        = var.talos_cp_01_ip
+  node                        = var.talos_cp_01.ip
+}
+
+resource "talos_machine_configuration_apply" "cp_02_config_apply" {
+  depends_on                  = [ proxmox_virtual_environment_vm.talos_cp_02, talos_machine_configuration_apply.cp_01_config_apply ]
+  client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.machineconfig_cp.machine_configuration
+  count                       = 1
+  node                        = var.talos_cp_02.ip
 }
 
 data "talos_machine_configuration" "machineconfig_worker" {
   cluster_name     = var.talos_cluster_name
-  cluster_endpoint = "https://${var.talos_cp_01_ip}:6443"
+  cluster_endpoint = "https://istari.benjaminmnoer.dk"
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
 
-resource "talos_machine_configuration_apply" "worker_config_apply" {
-  depends_on                  = [ proxmox_virtual_environment_vm.talos_worker_01 ]
+resource "talos_machine_configuration_apply" "worker_01_config_apply" {
+  depends_on                  = [ proxmox_virtual_environment_vm.talos_worker_01, talos_machine_configuration_apply.cp_02_config_apply ]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.machineconfig_worker.machine_configuration
   count                       = 1
-  node                        = var.talos_worker_01_ip
+  node                        = var.talos_worker_01.ip
+}
+
+resource "talos_machine_configuration_apply" "worker_02_config_apply" {
+  depends_on                  = [ proxmox_virtual_environment_vm.talos_worker_02, talos_machine_configuration_apply.worker_01_config_apply ]
+  client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.machineconfig_worker.machine_configuration
+  count                       = 1
+  node                        = var.talos_worker_02.ip
 }
 
 resource "talos_machine_bootstrap" "bootstrap" {
-  depends_on           = [ talos_machine_configuration_apply.cp_config_apply ]
+  depends_on           = [ talos_machine_configuration_apply.worker_02_config_apply ]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  node                 = var.talos_cp_01_ip
+  node                 = var.talos_cp_01.ip
 }
 
 data "talos_cluster_health" "health" {
-  depends_on           = [ talos_machine_configuration_apply.cp_config_apply, talos_machine_configuration_apply.worker_config_apply ]
+  depends_on           = [ talos_machine_bootstrap.bootstrap ]
   client_configuration = data.talos_client_configuration.talosconfig.client_configuration
-  control_plane_nodes  = [ var.talos_cp_01_ip ]
-  worker_nodes         = [ var.talos_worker_01_ip ]
+  control_plane_nodes  = [ var.talos_cp_01.ip, var.talos_cp_02.ip ]
+  worker_nodes         = [ var.talos_worker_01.ip, var.talos_worker_02.ip ]
   endpoints            = data.talos_client_configuration.talosconfig.endpoints
 }
 
-data "talos_cluster_kubeconfig" "kubeconfig" {
+resource "talos_cluster_kubeconfig" "kubeconfig" {
   depends_on           = [ talos_machine_bootstrap.bootstrap, data.talos_cluster_health.health ]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
-  node                 = var.talos_cp_01_ip
+  node                 = var.talos_cp_01.ip
 }
 
 output "talosconfig" {
@@ -62,6 +78,6 @@ output "talosconfig" {
 }
 
 output "kubeconfig" {
-  value = data.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
+  value = talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
   sensitive = true
 }
--- terraform/talos/virtualmachines.tf
+++ terraform/talos/virtualmachines.tf
@@ -1,44 +1,91 @@
 resource "proxmox_virtual_environment_vm" "talos_cp_01" {
-  name        = "talos-cp-01"
-  description = "Managed by Terraform"
+  name        = "${var.talos_cp_01.name}"
+  description = "${var.talos_node_defaults.description}"
   tags        = ["terraform","talos","controlplane"]
-  node_name   = "tower"
+  node_name   = "${var.talos_cp_01.node}"
   on_boot     = true
 
   cpu {
-    cores = 2
-    type = "x86-64-v2-AES"
+    cores = var.talos_cp_01.cpu
+    type = "${var.talos_node_defaults.cputype}"
   }
 
   memory {
-    dedicated = 4096
+    dedicated = var.talos_cp_01.memory
   }
 
   agent {
-    enabled = true
+    enabled = var.talos_node_defaults.agent
   }
 
   network_device {
-    bridge = "vmbr0"
+    bridge = "${var.talos_node_defaults.network_device}"
   }
 
   disk {
-    datastore_id = "unraid-domains"
-    file_id      = "unraid-isos:iso/talos-v1.9.2-nocloud-amd64.img"
-    file_format  = "raw"
-    interface    = "virtio0"
-    size         = 20
+    datastore_id = "${var.talos_node_defaults.disk.datastore_id}"
+    file_id      = "${var.talos_node_defaults.disk.file_id}"
+    file_format  = "${var.talos_node_defaults.disk.file_format}"
+    interface    = "${var.talos_node_defaults.disk.interface}"
+    size         = var.talos_cp_01.disksize
   }
 
   operating_system {
-    type = "l26" # Linux Kernel 2.6 - 5.X.
+    type = "${var.talos_node_defaults.operating_system}" # Linux Kernel 2.6 - 5.X.
   }
 
   initialization {
     datastore_id = "local"
     ip_config {
       ipv4 {
-        address = "${var.talos_cp_01_ip}/24"
+        address = "${var.talos_cp_01.ip}/24"
+        gateway = var.default_gateway
+      }
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "talos_cp_02" {
+  name        = "${var.talos_cp_02.name}"
+  description = "${var.talos_node_defaults.description}"
+  tags        = ["terraform","talos","controlplane"]
+  node_name   = "${var.talos_cp_02.node}"
+  on_boot     = true
+
+  cpu {
+    cores = var.talos_cp_02.cpu
+    type = "${var.talos_node_defaults.cputype}"
+  }
+
+  memory {
+    dedicated = var.talos_cp_02.memory
+  }
+
+  agent {
+    enabled = var.talos_node_defaults.agent
+  }
+
+  network_device {
+    bridge = "${var.talos_node_defaults.network_device}"
+  }
+
+  disk {
+    datastore_id = "${var.talos_node_defaults.disk.datastore_id}"
+    file_id      = "${var.talos_node_defaults.disk.file_id}"
+    file_format  = "${var.talos_node_defaults.disk.file_format}"
+    interface    = "${var.talos_node_defaults.disk.interface}"
+    size         = var.talos_cp_02.disksize
+  }
+
+  operating_system {
+    type = "${var.talos_node_defaults.operating_system}" # Linux Kernel 2.6 - 5.X.
+  }
+
+  initialization {
+    datastore_id = "local"
+    ip_config {
+      ipv4 {
+        address = "${var.talos_cp_02.ip}/24"
         gateway = var.default_gateway
       }
     }
@@ -46,47 +93,93 @@ resource "proxmox_virtual_environment_vm" "talos_cp_01" {
 }
 
 resource "proxmox_virtual_environment_vm" "talos_worker_01" {
-  depends_on = [ proxmox_virtual_environment_vm.talos_cp_01 ]
-  name        = "talos-worker-01"
-  description = "Managed by Terraform"
-  tags        = ["terraform", "talos", "worker"]
-  node_name   = "tower"
+  name        = "${var.talos_worker_01.name}"
+  description = "${var.talos_node_defaults.description}"
+  tags        = ["terraform","talos","controlplane"]
+  node_name   = "${var.talos_worker_01.node}"
   on_boot     = true
 
   cpu {
-    cores = 4
-    type = "x86-64-v2-AES"
+    cores = var.talos_worker_01.cpu
+    type = "${var.talos_node_defaults.cputype}"
   }
 
   memory {
-    dedicated = 4096
+    dedicated = var.talos_worker_01.memory
   }
 
   agent {
-    enabled = true
+    enabled = var.talos_node_defaults.agent
   }
 
   network_device {
-    bridge = "vmbr0"
+    bridge = "${var.talos_node_defaults.network_device}"
   }
 
   disk {
-    datastore_id = "unraid-domains"
-    file_id      = "unraid-isos:iso/talos-v1.9.2-nocloud-amd64.img"
-    file_format  = "raw"
-    interface    = "virtio0"
-    size         = 20
+    datastore_id = "${var.talos_node_defaults.disk.datastore_id}"
+    file_id      = "${var.talos_node_defaults.disk.file_id}"
+    file_format  = "${var.talos_node_defaults.disk.file_format}"
+    interface    = "${var.talos_node_defaults.disk.interface}"
+    size         = var.talos_worker_01.disksize
   }
 
   operating_system {
-    type = "l26" # Linux Kernel 2.6 - 5.X.
+    type = "${var.talos_node_defaults.operating_system}" # Linux Kernel 2.6 - 5.X.
   }
 
   initialization {
     datastore_id = "local"
     ip_config {
       ipv4 {
-        address = "${var.talos_worker_01_ip}/24"
+        address = "${var.talos_worker_01.ip}/24"
+        gateway = var.default_gateway
+      }
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "talos_worker_02" {
+  name        = "${var.talos_worker_02.name}"
+  description = "${var.talos_node_defaults.description}"
+  tags        = ["terraform","talos","controlplane"]
+  node_name   = "${var.talos_worker_02.node}"
+  on_boot     = true
+
+  cpu {
+    cores = var.talos_worker_02.cpu
+    type = "${var.talos_node_defaults.cputype}"
+  }
+
+  memory {
+    dedicated = var.talos_worker_02.memory
+  }
+
+  agent {
+    enabled = var.talos_node_defaults.agent
+  }
+
+  network_device {
+    bridge = "${var.talos_node_defaults.network_device}"
+  }
+
+  disk {
+    datastore_id = "${var.talos_node_defaults.disk.datastore_id}"
+    file_id      = "${var.talos_node_defaults.disk.file_id}"
+    file_format  = "${var.talos_node_defaults.disk.file_format}"
+    interface    = "${var.talos_node_defaults.disk.interface}"
+    size         = var.talos_worker_02.disksize
+  }
+
+  operating_system {
+    type = "${var.talos_node_defaults.operating_system}" # Linux Kernel 2.6 - 5.X.
+  }
+
+  initialization {
+    datastore_id = "local"
+    ip_config {
+      ipv4 {
+        address = "${var.talos_worker_02.ip}/24"
         gateway = var.default_gateway
       }
     }
