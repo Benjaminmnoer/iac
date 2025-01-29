bcc44dfb2b7a2fc52be1fe5f74ee12a8ee5aa373
Benjamin Noer <benjaminmnoer25@gmail.com>
2025-01-29 14:07:45 +0100

    Updated dev machine definition

--- terraform/proxmox/virtualmachines.tf
+++ terraform/proxmox/virtualmachines.tf
@@ -1,50 +1,85 @@
+resource "proxmox_virtual_environment_file" "arch_user_data_cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "pve1"
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    users:
+      - default
+      - name: benjaminmn
+        groups:
+          - wheel
+        shell: /bin/bash
+        ssh_authorized_keys:
+          - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFoD5D17uiVWei7OuHjo4P98yMfjP6vrLPcG3MgGLEu benjaminmnoer25@gmail.com
+        sudo: ALL=(ALL) NOPASSWD:ALL
+      - name: ansible
+        groups:
+          - wheel
+        shell: /bin/bash
+        ssh_authorized_keys:
+          - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSv5Qo/+Z5DIWkmlFz/Fw+TeUbIfVC3sBcwhHzec8Im ansible
+        sudo: ALL=(ALL) NOPASSWD:ALL
+    runcmd:
+        - pacman -Syyuu --noconfirm
+        - pacman -S --noconfirm qemu-guest-agent net-tools
+        - timedatectl set-timezone Europe/Copenhagen
+        - systemctl enable qemu-guest-agent
+        - systemctl start qemu-guest-agent
+        - echo "done" > /tmp/cloud-config.done
+    EOF
+
+    file_name = "arch-user-data-cloud-config.yaml"
+  }
+}
+
 resource "proxmox_virtual_environment_vm" "arch_dev_01" {
-  name        = var.arch_dev_01.name
-  description = var.arch_dev_01.description
-  tags        = ["terraform","talos","controlplane"]
-  node_name   = var.arch_dev_01.node
+  depends_on = [ proxmox_virtual_environment_file.arch_user_data_cloud_config ]
+  name        = "arch-dev-01"
+  description = "Arch Linux dev machine. Managed by Terraform."
+  tags        = ["terraform", "arch", "dev"]
+  node_name   = "pve1"
   on_boot     = true
 
   cpu {
-    cores = var.arch_dev_01.cpu
-    type = var.arch_dev_01.cputype
+    cores = 8
+    type  = "host"
   }
 
   memory {
-    dedicated = var.arch_dev_01.memory
+    dedicated = 8192
   }
 
   agent {
-    enabled = var.arch_dev_01.agent
+    enabled = true
   }
 
   network_device {
-    bridge = var.arch_dev_01.network_device
+    bridge = "vmbr0"
   }
 
   disk {
-    datastore_id = var.arch_dev_01.disk.datastore_id
-    file_id      = var.arch_dev_01.disk.file_id
-    file_format  = var.arch_dev_01.disk.file_format
-    interface    = var.arch_dev_01.disk.interface
-    size         = var.arch_dev_01.disk.size
+    datastore_id = "data-nvme0n1"
+      file_id      = "unraid-isos:iso/arch-linux-amd64-cloudimg-20250115.img"
+      file_format  = "qcow2"
+      interface    = "virtio0"
+      size         = 100
   }
 
   operating_system {
-    type = "${var.arch_dev_01.operating_system}" # Linux Kernel 2.6 - 5.X.
+    type = "l26"
   }
 
   initialization {
     datastore_id = "local"
     ip_config {
       ipv4 {
-        address = "${var.arch_dev_01.ip}/24"
+        address = "192.168.50.5/24"
         gateway = var.default_gateway
       }
     }
-    user_account {
-      username = "benjaminmn"
-      keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFoD5D17uiVWei7OuHjo4P98yMfjP6vrLPcG3MgGLEu benjaminmnoer25@gmail.com" ]
-    }
+    user_data_file_id = proxmox_virtual_environment_file.arch_user_data_cloud_config.id
   }
 }
