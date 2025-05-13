42475c388c86c7b29b081340f6395c1ee06cf3a4
Benjamin Noer <benjaminmnoer25@gmail.com>
2025-05-13 19:20:58 +0200

    Proxmox base provisioning

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -1,27 +1,31 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
-      version = "0.70.0"
+      source  = "bpg/proxmox"
+      version = "0.77.0"
     }
   }
-  backend "pg" {
-    conn_str = "postgres://192.168.50.104/terraform"
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate27519"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "proxmox" {
-  endpoint = var.virtual_environment_endpoint
-  username = var.virtual_environment_username
-  password = var.virtual_environment_password
+  endpoint  = var.virtual_environment_endpoint
+  api_token = var.virtual_environment_api_token
 
   min_tls = 1.2
 
   # uncomment (unless on Windows...)
-  tmp_dir  = "/var/tmp"
+  tmp_dir = "/var/tmp"
+  insecure = true
 
   ssh {
-    agent = true
+    agent       = true
+    username    = "terraform"
     private_key = file("~/.ssh/terraform_id_25519")
   }
 }
--- terraform/proxmox/virtualmachines.tf
+++ terraform/proxmox/virtualmachines.tf
@@ -1,7 +1,7 @@
 resource "proxmox_virtual_environment_file" "arch_user_data_cloud_config" {
   content_type = "snippets"
   datastore_id = "local"
-  node_name    = "pve1"
+  node_name    = "northrend"
 
   source_raw {
     data = <<-EOF
@@ -15,19 +15,11 @@ resource "proxmox_virtual_environment_file" "arch_user_data_cloud_config" {
         ssh_authorized_keys:
           - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFoD5D17uiVWei7OuHjo4P98yMfjP6vrLPcG3MgGLEu benjaminmnoer25@gmail.com
         sudo: ALL=(ALL) NOPASSWD:ALL
-      - name: ansible
-        groups:
-          - wheel
-        shell: /bin/bash
-        ssh_authorized_keys:
-          - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSv5Qo/+Z5DIWkmlFz/Fw+TeUbIfVC3sBcwhHzec8Im ansible
-        sudo: ALL=(ALL) NOPASSWD:ALL
     runcmd:
         - pacman -Syyuu --noconfirm
         - pacman -S --noconfirm qemu-guest-agent net-tools
         - timedatectl set-timezone Europe/Copenhagen
-        - systemctl enable qemu-guest-agent
-        - systemctl start qemu-guest-agent
+        - systemctl enable qemu-guest-agent --now
         - echo "done" > /tmp/cloud-config.done
     EOF
 
@@ -35,13 +27,13 @@ resource "proxmox_virtual_environment_file" "arch_user_data_cloud_config" {
   }
 }
 
-resource "proxmox_virtual_environment_vm" "arch_dev_01" {
+resource "proxmox_virtual_environment_vm" "runeforge" {
   depends_on  = [proxmox_virtual_environment_file.arch_user_data_cloud_config]
-  name        = "arch-dev-01"
+  name        = "runeforge"
   description = "Arch Linux dev machine. Managed by Terraform."
   tags        = ["terraform", "arch", "dev"]
-  node_name   = "pve1"
-  on_boot     = true
+  node_name   = "northrend"
+  on_boot     = false
 
   cpu {
     cores = 8
@@ -58,10 +50,11 @@ resource "proxmox_virtual_environment_vm" "arch_dev_01" {
 
   network_device {
     bridge = "vmbr0"
+    vlan_id = 3
   }
 
   disk {
-    datastore_id = "data-nvme0n1"
+    datastore_id = "data"
     file_id      = "unraid-isos:iso/arch-linux-amd64-cloudimg-20250115.img"
     file_format  = "qcow2"
     interface    = "virtio0"
@@ -73,11 +66,11 @@ resource "proxmox_virtual_environment_vm" "arch_dev_01" {
   }
 
   initialization {
-    datastore_id = "local"
+    datastore_id = "data"
     ip_config {
       ipv4 {
-        address = "192.168.50.5/24"
-        gateway = var.default_gateway
+        address = "192.168.3.2/24"
+        gateway = "192.168.3.1"
       }
     }
     user_data_file_id = proxmox_virtual_environment_file.arch_user_data_cloud_config.id
